49f175fdf906d8b597b86e1599be684c757a7f34
Ingo Schommer <me@chillu.com>
2020-06-19 13:23:50 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Silverstripe Module Issue Browser Util
+
+This utility reads a `composer.lock` file,
+discovers any Silverstripe modules contained there,
+and opens the [Silverstripe Issue Browser](http://silverstripe-issue-tracker.silverstripe.org) app.
+It shows only issues related to those Silverstripe modules.
+
+## Installation
+
+Install within a project (auto-discovers lock file)
+
+```
+composer require silverstripe/github-issue-search-composer-util
+``` 
+
+Install globally (and pass through a lock file)
+
+```
+composer global require silverstripe/github-issue-search-composer-util
+``` 
+
+## Usage
+
+In a Silverstripe project:
+
+```
+vendor/bin/github-issue-search
+```
+
+Globally with a custom lock file:
+
+```
+cat /my/project/composer.lock | github-issue-search
+```
+
+The command will output a URL that you can open in your favourite browser.
+
+On MacOSX, you can do this automatically with the `open` command,
+on Windows with the `start` command.
+
+```
+vendor/bin/github-issue-search | open
+``` 
---
+++ src/UrlGenerator.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Silverstripe\GithubIssueSearch\Util;
+
+/**
+ * Could've done this with composer/composer package,
+ * but seemed a bit overkill :D
+ */
+class UrlGenerator
+{
+    /**
+     * @var string
+     */
+    protected $urlTemplate = 'https://silverstripe-issue-tracker.silverstripe.org?customRepos=%s';
+
+    /**
+     * @param string $lockfileStr
+     * @return string URL
+     */
+    public function generate(string $lockfileStr)
+    {
+        $data = json_decode($lockfileStr, true);
+
+        $urls = array_filter(array_map(function ($package) {
+            if (!$this->isSilverstripeRepo($package) || !$this->isGithubRepo($package)) {
+                return null;
+            }
+
+            return $package['source']['url'];
+        }, $data['packages']));
+
+        // Assuming Github URLs, the "identifier" is the same as the path name
+        $ids = array_filter(array_map(function($url) {
+            return $this->extractRepoIdentifier($url);
+        }, $urls));
+
+        return sprintf($this->urlTemplate, implode(',', $ids));
+    }
+
+    protected function isSilverstripeRepo($package)
+    {
+        $types = [
+            'silverstripe-module',
+            'silverstripe-vendormodule',
+            'silverstripe-recipe'
+        ];
+        return in_array($package['type'], $types);
+    }
+
+    protected function isGithubRepo($package)
+    {
+        if (!isset($package['source']['url'])) {
+            return false;
+        }
+
+        $url = $package['source']['url'];
+
+        return (
+            preg_match('#^https?://github.com#', $url) ||
+            preg_match('#^https?://www.github.com#', $url)
+        );
+    }
+
+    protected function extractRepoIdentifier($url)
+    {
+        return trim(preg_replace('/\.git$/', '', parse_url($url, PHP_URL_PATH)), '/');
+    }
+}
